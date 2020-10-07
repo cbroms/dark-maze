f152ded2f6f2a6593a4462d3465ef8f01f5bde98
hitechlife <kanjur@andrew.cmu.edu>
2020-10-07 17:00:00 -0500

    Added request logic and TODOs, need to add rooms

--- server.js
+++ server.js
@@ -28,12 +28,15 @@ const map = [
 //TODO: define this in a different way
 const MAX_PAYLOADS = map.length / 2;
 const PAYLOAD_NODE = 0;
+const WIN_PAYLOADS = MAX_PAYLOADS - 1;
 
 // the game state
 var state = {
   players: {},
   payloads: [],
+  payloads_brought = 0,
   gameTimer: -1,
+  gameOver: false,
 };
 
 // for reference
@@ -46,8 +49,24 @@ var state = {
 //   coolTimer: bool, // if player is on cooldown
 // };
 
-// EVENTS FROM CLIENT:
-// "move" --> {node: int}
+/* TODO client side
+   --handle rendering different map views based on if player.isBad is true
+   --handle receiving "map" w/map object
+   --handle receiving "state"
+   --handle checking if player is on same node as IT:if so send "badPlayerNode"
+     event to server
+   --handle checking if player is NOT carrying a payload and is on same node
+     as a payload (and NOT same node as IT), then send "getPayload" event to 
+     server
+   --render when a player is carrying a payload by checking hasPayload
+   --render player movement cooldown by checking if coolTimer > 0 and make sure
+     adjacent nodes aren't highlighted until coolTimer is back to 0
+   --render player movement on specific EDGE by lighting up edge if 
+     moveTimer > 0 and a player is on it; make sure adjacent nodes aren't 
+     highlighted in red until moveTimer is back to 0
+   --if state.gameOver, ignore all player input and do NOTHING 
+   --render text/img for "payloadsBrought" on UI (num payloads brought to center), game over, game start etc (currently not handled, just written as notes in this file)
+*/
 
 io.on("connection", (socket) => {
   console.log("A user connected");
@@ -76,7 +95,7 @@ io.on("connection", (socket) => {
         " players"
     );
 
-    // TODO: send the map to the client
+    // send the map to the client
     socket.emit("map", map);
 
     // Start the game when we have 4 players
@@ -85,7 +104,7 @@ io.on("connection", (socket) => {
     }
   } else {
     socket.emit("message", "sorry, game is currently full");
-    // TODO create a new "room" that acts as a new game for the next set of players
+    // TODO: create a new "room" that acts as a new game for the next set of players
     // https://socket.io/docs/rooms/
   }
 
@@ -94,61 +113,96 @@ io.on("connection", (socket) => {
   });
 
   // Handle user requests here
-  socket.on("action", function (obj) {
-    //respawn payload elsewhere + set hasLoad to false whenever "it" touches player
-    //keep track of how many on map at given time
-    //TODO check if player is currently moving; if so don't highlight other movement nodes + don't let them click anything
-    //TODO if player touches payload set "hasPayload" to true
-    //TODO check if player is on cooldown before letting them click
-    //TODO check if player is currently moving; if so don't highlight other movement nodes + don't let them click anything
+  socket.on("move", function (node) {
+    // If player is currently moving or on cooldown, nothing happens
+    if (moveTimer > 0 || coolTimer > 0) {
+      return;
+    }
+
+    // IF move not valid, do nothing
+    //TODO: add UI text for invalid move?
+    if (!(map[state.players[socket.id].playerNode].includes(node) === true)) {
+      return;
+    }
+
+    state.players[socket.id].playerEdge = ??;
+    state.players[socket.id].moveTimer = MAX_MOVE;
+    state.players[socket.id].playerNode = node;
+  });
+
+  // If some player on same node as "IT"
+  socket.on("badPlayerNode", function (obj) {
+    // just mark the player as not-carrying and spawn a new payload
+    state.players[socket.id].hasPayload = false;
+    generatePayloads(1);
+  });
+
+  // If some player on same node as a payload
+  socket.on("getPayload", function (obj) {
+    // If they're carrying a payload, do nothing
+    // (checked on client side) If on same node as "it", do nothing
+    if (state.players[socket.id].hasPayload === true) return;
+    state.players[socket.id].hasPayload = true;
   });
 
   //TODO: do we want an onDisconnect?
-});
+}); // end onConnect
 
-  // setInterval works in milliseconds
-  setInterval(function () {
-    // Do processing of game state
-    for (var playerId in state.players) {
-      var playerState = state.players[playerId];
-      // Cooldown on movement
-      if (playerState.coolTimer > 0) {
-        playerState.coolTimer -= 1;
-      }
+// setInterval works in milliseconds
+setInterval(function () {
+  // Do processing of game state
+  for (var playerId in state.players) {
+    var playerState = state.players[playerId];
+    // Cooldown on movement
+    if (playerState.coolTimer > 0) {
+      playerState.coolTimer -= 1;
+    }
 
-      // Player is moving
-      if (playerState.moveTimer > 0) {
-        playerState.moveTimer -= 1;
-        if (playerState.moveTimer === 0) {
-          playerState.coolTimer = MAX_COOLDOWN;
-        }
+    // Player is moving
+    if (playerState.moveTimer > 0) {
+      playerState.moveTimer -= 1;
+      if (playerState.moveTimer === 0) {
+        playerState.coolTimer = MAX_COOLDOWN;
       }
     }
 
-    if (state.gameTimer > 0) state.gameTimer -= 1;
-    else if (state.gameTimer === 0) {
-      endGame();
+    // Check if player on same node as payload_node and if so add
+    if (playerState.moveTimer <= 0 && playerState.hasPayload && playerNode === PAYLOAD_NODE) {
+      state.payloads_brought++;
+      if (state.payloads_brought === WIN_PAYLOADS) {
+        endGame();
+      }
     }
-    // if gameTimer is -1 then we haven't started yet
+  }
 
-    //TODO stop game if timer reaches 0
+  if (state.gameTimer > 0) state.gameTimer -= 1;
+  else if (state.gameTimer === 0) {
+    endGame();
+  }
+  // if gameTimer is -1 then we haven't started yet, so ignore if timer < 0
 
-    // Send the state to all players
-    io.sockets.emit("state", state);
-  }, UPDATE_TIME);
+  // Send the state to all players
+  io.sockets.emit("state", state);
+}, UPDATE_TIME);
 
 function endGame() {
   // prevent any further user actions
-  // check # payloads in center node to see who won
-  // display winner text
-  // reset server for new game once all disconnect
+  state.gameOver = true;
   state.gameTimer = -1;
+
+  // TODO: display winner text on client side
+    // winner = state.payloads_brought === WIN_PAYLOADS ? "Good" : "Bad";
+    // winnerText = winner + " team won!";
+  // TODO: reset server room for new game once all disconnect
 }
 
 function startGame() {
-  //TODO render some "game start" text on the client side/confirmation
-  // show # of payloads brought to center in a text??
+  console.log("Game start");
+  //TODO: render some "game start" text on the client side/confirmation
+  // show UI text for payloads_brought
   state.gameTimer = ROUND_TIMING;
+  state.gameOver = false;
+  state.payloads_brought = 0;
 
   // Initialize payload array to false
   for (let i = 0; i < map.length; i++) {
